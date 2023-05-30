0c6365ab420fe64cfd0ed09f883360da535309d4
kristofferv98 <kristoffer.vatnehol@gmail.com>
2023-05-30 12:59:37 +0200

    Initial Commit

---
+++ Matplanleggern2/Matplanleggern2App.swift
@@ -0,0 +1,17 @@
+//
+//  Matplanleggern2App.swift
+//  Matplanleggern2
+//
+//  Created by Kristoffer Vatnehol on 30/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct Matplanleggern2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
